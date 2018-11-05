84e4af107a74673524ccaa1f58c746de8fc17fe8
Minh Hoang Nhat Tran <35718590+hoangtnm@users.noreply.github.com>
2018-11-05 21:16:39 +0700

    Update capture_video_from_camera.py

--- Tutorials/capture_video_from_camera.py
+++ Tutorials/capture_video_from_camera.py
@@ -1,19 +1,25 @@
 import numpy as np
 import cv2
 
+"""
+To capture a video, you need to create a VideoCapture object
+Its argument can be either the device index or the name of a video file.
+After that, you can capture frame-by-frame. But at the end, don’t forget to release the capture
+"""
+
 cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+while(cap.isOpened()):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+	
+	# Our operations on the frame come here
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	
+	# Display the resulting frame
+	cv2.imshow('frame',gray)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
 # When everything done, release the capture
 cap.release()
