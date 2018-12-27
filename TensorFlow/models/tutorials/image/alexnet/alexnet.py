66f0a05fd8e70818de8b388ded0874b665578dda
hoangtnm <minhhoangtrannhat@gmail.com>
2018-12-27 23:57:40 +0700

    Refactor to PEP 498

--- TensorFlow/models/tutorials/image/alexnet/alexnet.py
+++ TensorFlow/models/tutorials/image/alexnet/alexnet.py
@@ -46,7 +46,7 @@ def _activation_summary(x):
     """
     # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
     # session. This helps the clarity of presentation on tensorboard.
-    tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
+    tensor_name = re.sub(f'{TOWER_NAME}_[0-9]*/', '', x.op.name)
     tf.summary.histogram(tensor_name + '/activations', x)
     tf.summary.scalar(tensor_name + '/sparsity',
                       tf.nn.zero_fraction(x))
