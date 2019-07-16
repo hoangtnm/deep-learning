59a7f637183d41659b5e93d1c98e06c9dc5c542f
hoangtnm <minhhoangtrannhat@gmail.com>
2019-07-16 22:46:16 +0700

    Add auto_car_jetson

---
+++ experiments/auto_car_jetson/basic-control.py
@@ -0,0 +1,137 @@
+# import sys, os
+# print (os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import cv2
+import time
+import argparse
+import pygame
+import numpy as np
+from imutils.video import WebcamVideoStream
+from controller import PS4Controller
+from controller import get_button_command
+from gpio_controller.ServoController import ServoController
+
+
+def run(speed_add=1, theta_add=20, controller='ps4'):
+    servo_controller = ServoController()
+    servo_controller.init()
+    
+    speed = 0
+    theta = 0
+    oldSpeed = 0
+    oldTheta = 0
+    max_speed = 30
+
+    stream = WebcamVideoStream(1).start()
+
+    # Initializing controller
+    if controller == 'ps4':
+        ps4_controller = PS4Controller()
+        ps4_controller.start()
+    else:
+        controller = 'keyboard'
+    
+    while True:
+        brake = False
+        frame = stream.read()
+        frame_height, frame_width, _ = frame.shape
+        resized_frame = cv2.resize(frame, (frame_width//2, frame_height//2))
+
+        cv2.putText(frame, f'Speed: {speed}, Theta: {theta}',
+                    (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+        cv2.imshow('frame', resized_frame)
+
+        button_data, axis_data, _ = ps4_controller.read()
+        is_command, button_num = get_button_command(button_data, controller=ps4_controller.controller)
+        ascii_code  = cv2.waitKey(1) & 0xFF
+
+        """PS4 Controller's axes map
+
+                     Axis 1                  Axis 5
+                    
+                      -1                      -1
+                       ^                       ^
+                       |                       |
+        Axis 0  -1 <---0---> 1   Axis 2 -1 <---0---> 1
+                       |                       |
+                       v                       v
+                       1                       1
+        """
+
+        raw_speed = axis_data[1]
+        raw_theta = axis_data[2]
+        if max_speed > 50:
+            max_speed = 50
+        elif max_speed < 0:
+            max_speed = 30
+
+        speed = np.interp(-raw_speed, (-1, 1), (-max_speed, max_speed))
+        theta = np.interp(raw_theta, (-1, 1), (-90, 90))
+
+        if ascii_code == ord('q'):
+            servo_controller.set_speed(0)
+            servo_controller.set_steer(0)
+            break
+        
+        elif (is_command == True):
+            if button_num == 0:
+                theta -= theta_add
+            elif button_num == 1:
+                brake = True
+            elif button_num == 2:
+                theta += theta_add
+            # elif  button_num == 4:
+            #     max_speed -= 5 * speed_add
+            # elif button_num == 5:
+            #     max_speed += 5 * speed_add
+            elif  button_num == 6:
+                speed -= speed_add
+            elif button_num == 7:
+                speed += speed_add
+        
+        elif ((ascii_code == ord('w')) or (ascii_code == ord('W'))):
+            speed += speed_add
+                    
+        elif ((ascii_code == ord('s')) or (ascii_code == ord('S'))):
+            speed -= speed_add
+
+        elif ((ascii_code == ord('d')) or (ascii_code == ord('D'))):
+            theta += theta_add
+
+        elif ((ascii_code == ord('a')) or (ascii_code == ord('A'))):
+            theta -= theta_add
+
+        elif ((ascii_code == ord('\n')) or (ascii_code == ord('\r'))):
+            brake = True
+
+                
+        if ((oldSpeed != speed) or (oldTheta != theta)):
+            oldSpeed = speed
+            oldTheta = theta
+
+            speed = servo_controller.set_speed(speed)
+            theta = servo_controller.set_steer(theta)
+        
+        if brake:
+            print("Brake")
+            speed = servo_controller.brake()
+            oldSpeed = speed
+            
+            time.sleep(0.5)
+
+    stream.stop()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--speed', default=1)
+    parser.add_argument('-t', '--theta', default=20)
+    parser.add_argument('-c', '--controller', default='ps4')
+
+    args = vars(parser.parse_args())
+    speed_add = args['speed']
+    theta_add = args['theta']
+    controller = args['controller']
+
+    run(float(speed_add), float(theta_add), controller=controller)
---
+++ experiments/auto_car_jetson/car/main.py
@@ -0,0 +1,183 @@
+import sys
+import torch
+import torchvision
+import numpy as np
+from board import SCL, SDA
+import busio
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import servo
+from adafruit_motor import motor
+from controller import PS4Controller
+import camera
+import cv2
+import csv
+from uuid import uuid1
+import neural_network
+
+
+class Autocar():
+
+    def __init__(self):
+        # init i2c
+        i2c = busio.I2C(SCL, SDA)
+
+        # init PCA
+        self.pca = PCA9685(i2c)
+        self.pca.frequency = 50
+
+        self.servo_steer = servo.Servo(self.pca.channels[0])
+        self.esc = servo.ContinuousServo(self.pca.channels[1])
+        
+        # init model
+        
+        model = neural_network.Net()
+        self.model = model.eval()
+        self.model.load_state_dict(torch.load('model/autopilot.pt'))
+        self.device = torch.device('cuda')
+        self.model.to(self.device)
+        
+        # init vars
+        self.temp = 0
+        mean = 255.0 * np.array([0.485, 0.456, 0.406])
+        stdev = 255.0 * np.array([0.229, 0.224, 0.225])
+        self.normalize = torchvision.transforms.Normalize(mean, stdev)
+        self.angle_out = 0
+
+        # init Camera
+        self.cam = camera.Camera()        
+
+        # initial content
+        with open('control_data.csv','w') as f:
+            f.write('date,steering,speed\n')
+        
+    def scale_servo(self, x):
+    
+        # used to scale -1,1 to 0,180
+        y = round((30-70)*x+1/1+1+70,2) 
+        
+        return y
+
+    def scale_esc(self, x):
+    
+        # used to scale -1,1 to 0,180
+        y = round((x+1)/12,2)
+        
+        return y
+        
+    def drive(self, axis_data):
+    
+        self.servo_steer.angle = self.scale_servo(-axis_data[0])
+        sum_inputs = round(-self.scale_esc(axis_data[4]) + self.scale_esc(axis_data[3]),2)
+        self.esc.throttle = sum_inputs
+
+    def save_data(self, axis_data):
+    
+        count = self.cam.count
+        img = self.cam.value
+
+        if count!= self.temp:
+        
+            num = uuid1()
+            cv2.imwrite('images/'+str(num)+".jpg", img)
+            
+            # append inputs to csv
+            with open('control_data.csv','a',newline='') as f:
+                writer=csv.writer(f)
+                writer.writerow([num,axis_data[0],axis_data[4]])
+                
+            self.temp = count
+            
+            print('Save data!')
+            
+        else:
+            pass
+        
+            
+    def preprocess(self, camera_value):
+    
+        x = camera_value
+        x = cv2.resize(x, (224, 224))
+        x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+        x = x.transpose((2, 0, 1))
+        x = torch.from_numpy(x).float()
+        x = self.normalize(x)
+        x = x.to(self.device)
+        x = x[None, ...]
+        
+        return x        
+    
+    def autopilot(self):
+        
+        img = self.preprocess(self.cam.value)
+        count = self.cam.count
+        
+        if count!= self.temp:
+            print('RUN!')
+            self.model.eval()
+            with torch.no_grad():
+                output = self.model(img)
+            outnump = output.cpu().data.numpy()
+            
+            if outnump >= 1:
+                self.angle_out = [[1]]
+                
+            elif outnump <= -1:
+                self.angle_out = [[-1]]
+            else:
+                self.angle_out = outnump
+                
+            print(self.angle_out[0][0])
+            
+            self.temp = count
+            
+            
+        else:
+            pass
+        
+        self.drive({0:self.angle_out[0][0],1:0.0,2:0.0,3:-1.0,4:1,5:0.0})
+        
+
+if __name__ == "__main__":
+
+    car = Autocar()
+    
+    # init controller
+    ps4 = PS4Controller()
+    ps4.init()
+    
+    # Start in training mode
+    train = True
+    trig = True
+    
+    def toggle(x):
+        return not x
+        
+    try:
+        while True:
+        
+            button_data, axis_data, hat_data = ps4.listen()
+            
+            if button_data[1] == True and trig:
+                train = toggle(train)
+                trig = False
+                
+            elif button_data[1] == False:
+                trig = True
+            else:
+                pass
+            
+            if train:
+                car.drive(axis_data)
+                
+                if axis_data[4] >= 0.12:
+                    car.save_data(axis_data)
+                else:
+                    print('Not saving img')
+            else:
+                car.autopilot()
+
+
+    except KeyboardInterrupt:
+            car.pca.deinit()
+            sys.exit(0)
+        
