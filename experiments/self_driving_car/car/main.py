d8ce0216f630cc1cbc97f5195f6cc5a35dd19b26
hoangtnm <minhhoangtrannhat@gmail.com>
2019-07-20 22:56:30 +0700

    Update main.py to pep8

--- experiments/self_driving_car/car/main.py
+++ experiments/self_driving_car/car/main.py
@@ -1,18 +1,19 @@
 import sys
+import cv2
+import csv
 import torch
 import torchvision
 import numpy as np
 import busio
-from board import SCL, SDA
+from board import SCL
+from board import SDA
+from uuid import uuid1
 from adafruit_pca9685 import PCA9685
 from adafruit_motor import servo
 from adafruit_motor import motor
 from controller import PS4Controller
 from controller import get_button_command
 import camera
-import cv2
-import csv
-from uuid import uuid1
 import neural_network
 
 
@@ -28,15 +29,14 @@ class Autocar():
 
         self.servo_steer = servo.Servo(self.pca.channels[0])
         self.esc = servo.ContinuousServo(self.pca.channels[1])
-        
+
         # init model
-        
         model = neural_network.Net()
         self.model = model.eval()
         self.model.load_state_dict(torch.load('model/autopilot.pt'))
         self.device = torch.device('cuda')
         self.model.to(self.device)
-        
+
         # init vars
         self.temp = 0
         mean = 255.0 * np.array([0.485, 0.456, 0.406])
@@ -45,57 +45,51 @@ class Autocar():
         self.angle_out = 0
 
         # init Camera
-        self.cam = camera.Camera()        
+        self.cam = camera.Camera()
 
         # initial content
-        with open('control_data.csv','w') as f:
+        with open('control_data.csv', 'w') as f:
             f.write('date,steering,speed\n')
-        
+
     def scale_servo(self, x):
-    
-        # used to scale -1,1 to 0,180
-        y = round((30-70)*x+1/1+1+70,2) 
-        
-        return y
+        """used to scale -1,1 to 0,180"""
+        return round((30-70)*x+1/1+1+70, 2)
 
     def scale_esc(self, x):
-    
-        # used to scale -1,1 to 0,180
-        y = round((x+1)/12,2)
-        
-        return y
-        
+        """used to scale -1,1 to 0,180"""
+        return round((x+1)/12, 2)
+
     def drive(self, axis_data):
-    
+
         self.servo_steer.angle = self.scale_servo(-axis_data[0])
-        sum_inputs = round(-self.scale_esc(axis_data[4]) + self.scale_esc(axis_data[3]),2)
+        sum_inputs = round(-self.scale_esc(axis_data[4]) +
+                           self.scale_esc(axis_data[3]), 2)
         self.esc.throttle = sum_inputs
 
     def save_data(self, axis_data):
-    
+
         count = self.cam.count
         img = self.cam.value
 
-        if count!= self.temp:
-        
+        if count != self.temp:
+
             num = uuid1()
             cv2.imwrite('images/'+str(num)+".jpg", img)
-            
+
             # append inputs to csv
-            with open('control_data.csv','a',newline='') as f:
-                writer=csv.writer(f)
-                writer.writerow([num,axis_data[0],axis_data[4]])
-                
+            with open('control_data.csv', 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow([num, axis_data[0], axis_data[4]])
+
             self.temp = count
-            
+
             print('Save data!')
-            
+
         else:
             pass
-        
-            
+
     def preprocess(self, camera_value):
-    
+
         x = camera_value
         x = cv2.resize(x, (224, 224))
         x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
@@ -104,71 +98,71 @@ class Autocar():
         x = self.normalize(x)
         x = x.to(self.device)
         x = x[None, ...]
-        
-        return x        
-    
+
+        return x
+
     def autopilot(self):
-        
+
         img = self.preprocess(self.cam.value)
         count = self.cam.count
-        
-        if count!= self.temp:
+
+        if count != self.temp:
             print('RUN!')
             self.model.eval()
             with torch.no_grad():
                 output = self.model(img)
             outnump = output.cpu().data.numpy()
-            
+
             if outnump >= 1:
                 self.angle_out = [[1]]
-                
+
             elif outnump <= -1:
                 self.angle_out = [[-1]]
             else:
                 self.angle_out = outnump
-                
+
             print(self.angle_out[0][0])
-            
+
             self.temp = count
-            
-            
+
         else:
             pass
-        
-        self.drive({0:self.angle_out[0][0],1:0.0,2:0.0,3:-1.0,4:1,5:0.0})
-        
+
+        self.drive({0: self.angle_out[0][0],
+                    1: 0.0, 2: 0.0, 3: -1.0, 4: 1, 5: 0.0})
+
 
 if __name__ == "__main__":
 
     car = Autocar()
-    
+
     # Initialize controller
     ps4_controller = PS4Controller()
     ps4_controller.start()
-    
+
     # Start in training mode
     train = True
     trig = True
-    
+
     def toggle(x):
         return not x
-        
+
     try:
         while True:
             button_data, axis_data, hat_data = ps4_controller.read()
-            
+
             if button_data[1] == True and trig:
                 train = toggle(train)
                 trig = False
-                
+
             elif button_data[1] == False:
                 trig = True
             else:
                 pass
-            
+
             if train:
                 car.drive(axis_data)
-                
+
                 if axis_data[4] >= 0.12:
                     car.save_data(axis_data)
                 else:
@@ -176,8 +170,6 @@ if __name__ == "__main__":
             else:
                 car.autopilot()
 
-
     except KeyboardInterrupt:
-            car.pca.deinit()
-            sys.exit(0)
-        
+        car.pca.deinit()
+        sys.exit(0)
