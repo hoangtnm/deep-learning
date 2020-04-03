f59b9a286c095f055ab0b846e4c0b1855dd05776
hoangtnm <minhhoangtrannhat@gmail.com>
2020-04-03 18:11:34 +0700

    Update TFC3D model and init LRMobileNetV2 model

--- research/action_recognition/models.py
+++ research/action_recognition/models.py
@@ -1,16 +1,16 @@
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.callbacks import (EarlyStopping, ModelCheckpoint,
-                                        TensorBoard)
-from tensorflow.keras.layers import (BatchNormalization, Conv3D, Dense,
-                                     Dropout, Flatten, MaxPool3D)
-from tensorflow.keras.models import Model, Sequential, load_model
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.layers import (GRU, LSTM, Conv3D, Dense, Dropout,
+                                     Flatten, GlobalAveragePooling2D,
+                                     MaxPool3D, TimeDistributed)
+from tensorflow.keras.models import Model, Sequential
 
 
 class TFC3D(Model):
 
     def __init__(self, num_classes: int, dropout: float = 0.5):
         super().__init__()
+        self.num_classes = num_classes
 
         # 1st group
         self.conv1a = Conv3D(64, (3, 3, 3), (1, 1, 1),
@@ -53,9 +53,9 @@ class TFC3D(Model):
         self.fc8 = Dense(num_classes, activation='softmax')
 
     @tf.function
-    def call(self, x, training: bool):
+    def call(self, x, training=False):
         x = self.conv1a(x)
-        x = self.pool1a(x)
+        x = self.pool1(x)
 
         x = self.conv2a(x)
         x = self.pool2(x)
@@ -77,3 +77,29 @@ class TFC3D(Model):
         x = self.dropout7(self.fc7(x), training=training)
         x = self.fc8(x)
         return x
+
+
+def get_LRMobileNetV2(frames: int, classes: int, input_shape=(224, 224, 3)):
+    """Long-term Recurrent MobileNetV2.
+
+    Args:
+        frames: number of frames in each sample.
+        classes: number of classes to classify frame sequences into.
+    """
+
+    # MobileNetV2 Feature Extractor
+    mobilenet = Sequential([
+        MobileNetV2(
+            input_shape, include_top=False, weights='imagenet'
+        ),
+        GlobalAveragePooling2D(),
+        Dense(1024)
+    ])
+
+    model = Sequential()
+    model.add(TimeDistributed(mobilenet, input_shape=(frames, 224, 224, 3)))
+    model.add(GRU(32, return_sequences=True))
+    model.add(GRU(32))
+    model.add(Dense(classes))
+
+    return model
