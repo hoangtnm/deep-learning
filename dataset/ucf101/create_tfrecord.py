f45eaa2efd8c8930d1a352db07526874eec9e97e
hoangtnm <minhhoangtrannhat@gmail.com>
2020-04-14 16:24:34 +0700

    Replace threading with multiprocessing

--- dataset/ucf101/create_tfrecord.py
+++ dataset/ucf101/create_tfrecord.py
@@ -3,9 +3,10 @@
 import argparse
 import json
 import logging
+import multiprocessing
 import os
 import random
-from threading import Thread
+from multiprocessing import Pool
 from typing import Dict, List, Tuple
 
 import cv2
@@ -28,7 +29,7 @@ def video_to_tensor(path: str) -> tf.Tensor:
         video_tensor (tf.Tensor): Tensor of frames.
 
     Shape:
-        - Output: (N, H_{out}, W_{out}, C_{out})
+        - Output: (N_{out}, H_{out}, W_{out}, C_{out})
     """
 
     frame_list = []
@@ -81,13 +82,13 @@ def serialize_example(video_tensor: tf.Tensor, label: int):
 
 
 def get_label_map_dict(label_list: List[str]) -> Dict[str, int]:
-    """Returns a dict mapping labels and the corresponding ids.
+    """Returns a dict mapping labels and the corresponding id.
 
     Args:
         label_list: List of label names.
 
     Returns:
-        label_map_dict: A dict mapping labels to the corresponding ids.
+        label_map_dict: A dict mapping labels to the corresponding id.
     """
 
     label_map_dict = {label: label_id
@@ -95,28 +96,29 @@ def get_label_map_dict(label_list: List[str]) -> Dict[str, int]:
     return label_map_dict
 
 
-def create_label_file(label_list: List[str], label_file: str) -> None:
-    """Creates a JSON dict of labels.
+def write_label_file(label_list: List[str], filename: str) -> None:
+    """Writes a JSON dict of labels to disk.
 
     Args:
         label_list: List of label names.
-        label_file: Path to write JSON file.
+        filename: Path to JSON file.
     """
 
     label_to_id = get_label_map_dict(label_list)
-    with open(label_file, 'w') as f:
+
+    with open(filename, 'w') as f:
         json.dump(label_to_id, f, indent=4)
 
 
 def get_dataset(data_dir: str, label_list: List[str]) -> List[Tuple[str, int]]:
-    """Returns list of examples, which is a tuple of path and label.
+    """Returns a list of examples, each of which is a tuple of path and label.
 
     Args:
         data_dir: Path to dataset directory.
         label_list: List of label names.
 
     Returns:
-        List of video info, which is a tuple of
+        List of examples. Each example is a tuple of
             video path, and the corresponding label.
     """
 
@@ -144,12 +146,14 @@ def get_dataset(data_dir: str, label_list: List[str]) -> List[Tuple[str, int]]:
     return list(zip(video_paths, labels))
 
 
-def split_dataset(dataset: List[Tuple[str, int]], num_splits: int) -> List[List[Tuple[str, int]]]:
-    """Splits dataset into shards.
+def split_dataset(dataset: List[Tuple[str, int]], num_splits: int) -> List[
+        List[Tuple[str, int]]]:
+    """Splits dataset to shards.
 
     Args:
-        dataset: List of examples. Each example is a tuple of path and label.
-        num_splits: How many subsets that the dataset will be splitted into.
+        dataset: List of examples. Each example is a tuple of
+            video path, and the corresponding label.
+        num_splits: How many shards that the dataset will be splitted to.
 
     Returns:
         shards: A list of shards. Each shard is a subset of the dataset.
@@ -158,8 +162,8 @@ def split_dataset(dataset: List[Tuple[str, int]], num_splits: int) -> List[List[
     shards = []
 
     spaced_indices = np.linspace(
-        0, len(dataset), num_splits+1).astype(np.uint)
-    shard_ranges = [[spaced_indices[idx], spaced_indices[idx+1]]
+        0, len(dataset), num_splits + 1).astype(np.uint)
+    shard_ranges = [[spaced_indices[idx], spaced_indices[idx + 1]]
                     for idx in range(len(spaced_indices) - 1)]
 
     for shard_idx, (start, end) in enumerate(shard_ranges):
@@ -169,58 +173,93 @@ def split_dataset(dataset: List[Tuple[str, int]], num_splits: int) -> List[List[
     return shards
 
 
-def write_tfrecord(thread_shard: List[Tuple[str, int]], filenames: List[str]):
-    """Writes shards into TFRecords.
+# def write_tfrecord(shard: List[Tuple[str, int]], filename: str):
+def write_tfrecord(info: Tuple[List[Tuple[str, int]], str]):
+    """Writes a shard to a TFRecords file.
 
     Args:
-        thread_shard: List of examples.
-            Each example is a tuple of `path` and `label`.
-        filenames: List of TFRecord filenames.
+        info: A tuple of dataset shard, and the corresponding filename.
+            shard: List of examples.
+                Each example is a tuple of `path` and `label`.
+            filename: Path to the TFRecords file.
     """
 
-    # Splits the dataset into shards.
-    shards = split_dataset(thread_shard, num_splits=len(filenames))
+    dataset, filename = info
 
-    for idx, path in enumerate(filenames):
-        writer = tf.io.TFRecordWriter(path)
-
-        for video_path, label in shards[idx]:
+    with tf.io.TFRecordWriter(filename) as writer:
+        for video_path, label in dataset:
             video_tensor = video_to_tensor(video_path)
             writer.write(serialize_example(video_tensor, label))
 
-        writer.close()
+
+# def process_dataset(dataset: List[Tuple[str, int]], num_shards: int,
+#                     num_threads: int, phase: str, output_dir: str):
+#     assert num_shards % num_threads == 0
+
+#     # Splits dataset N shards for each thread.
+#     # At this stage, N will be equal to num_threads.
+#     dataset = split_dataset(dataset, num_splits=num_threads)
+
+#     # Coordinates the termination of a set of threads.
+#     coord = tf.train.Coordinator()
+#     threads = []
+
+#     shards_per_thread = num_shards // num_threads
+
+#     for idx, thread_idx in enumerate(range(num_threads)):
+#         filenames = [
+#             os.path.join(
+#                 output_dir,
+#                 f'{phase}-{thread_idx * shards_per_thread + shard_idx}-of-{num_shards}'
+#             ) for shard_idx in range(shards_per_thread)
+#         ]
+
+#         sub_dataset = dataset[idx]
+#         t = Thread(target=write_tfrecord, args=(sub_dataset, filenames))
+#         t.start()
+#         threads.append(t)
+
+#     # Waits for all the threads to terminate.
+#     coord.join(threads)
 
 
-def process_dataset(dataset: List[Tuple[str, int]], num_shards: int,
-                    num_threads: int, phase: str, output_dir: str):
+def get_filenames(output_dir: str, phase: str, num_shards: int) -> List[str]:
+    """Returns a list of TFRecords filenames.
 
-    assert num_shards % num_threads == 0
+    Args:
+        phase: type of output file, e.g. `train` or `val`
+        num_shards: number of shards.
 
-    # Splits dataset N shards for each thread.
-    # At this stage, N will be equal to num_threads.
-    dataset = split_dataset(dataset, num_splits=num_threads)
+    Returns:
+        filenames: A list of TFRecords filenames.
+    """
 
-    # Coordinates the termination of a set of threads.
-    coord = tf.train.Coordinator()
-    threads = []
+    filenames = []
 
-    shards_per_thread = num_shards // num_threads
+    for idx in range(num_shards):
+        filename = f'{phase}-{idx:05d}-of-{num_shards:05d}'
+        filename = os.path.join(output_dir, filename)
+        filenames.append(filename)
 
-    for idx, thread_idx in enumerate(range(num_threads)):
-        filenames = [
-            os.path.join(
-                output_dir,
-                f'{phase}-{thread_idx*shards_per_thread+shard_idx}-of-{num_shards}'
-            ) for shard_idx in range(shards_per_thread)
-        ]
+    return filenames
 
-        sub_dataset = dataset[idx]
-        t = Thread(target=write_tfrecord, args=(sub_dataset, filenames))
-        t.start()
-        threads.append(t)
 
-    # Waits for all the threads to terminate.
-    coord.join(threads)
+def process_dataset_v2(shards: List[List[Tuple[str, int]]],
+                       filenames: List[str],
+                       num_processes: int):
+    """Writes dataset shards to TFRecords files.
+
+    Args:
+        shards: A list of shards, each of which is a list of examples.
+            Each example is a tuple of video path and the corresponding label.
+        filenames: A list of TFRecords filenames.
+        num_processes: Number of CPU cores to process in parallel.
+    """
+
+    args = tuple(zip(shards, filenames))
+
+    with Pool(num_processes) as pool:
+        pool.map(write_tfrecord, args)
 
 
 def main():
@@ -240,34 +279,41 @@ def main():
     #     required=True)
     parser.add_argument(
         '--num_train_shards',
-        default=32,
+        default=64,
         help='Number of shards for training',
         required=False)
     parser.add_argument(
         '--num_val_shards',
-        default=32,
+        default=64,
         help='Number of shards for validation',
         required=False)
     parser.add_argument(
-        '--num_threads',
-        default=4,
-        help='Number of threads to process in parallel',
+        '--num_cpu',
+        default=2,
+        type=int,
+        help='Number of CPUs to process in parallel',
         required=False
     )
 
     args = parser.parse_args()
 
     assert os.path.exists(args.input_dir)
+    assert args.num_train_shards >= args.num_cpu
+    assert args.num_val_shards >= args.num_cpu
+
+    if args.num_cpu != 1:
+        assert args.num_cpu <= multiprocessing.cpu_count()
+        assert args.num_cpu % 2 == 0, 'Number of CPU shoule be divisible by 2'
 
     label_list = sorted(os.listdir(args.input_dir))
     label_map_file = os.path.join(args.output_dir, 'label_map.txt')
-    create_label_file(label_list, label_map_file)
+    write_label_file(label_list, label_map_file)
 
     logging.basicConfig(level=logging.INFO)
 
     logger.info('***** Creating TFRecords *****')
     logger.info(f'  Num labels = {len(label_list)}')
-    logger.info(f'  Num threads = {args.num_threads}')
+    logger.info(f'  Num CPUs = {args.num_cpu}')
 
     # # Non-threaded method
     # writer = tf.io.TFRecordWriter(
@@ -288,14 +334,23 @@ def main():
     # writer.close()
     # # Non-threaded method
 
-    # Threaded method
     dataset = get_dataset(args.input_dir, label_list)
     trainset, valset = train_test_split(dataset, test_size=0.2)
-    process_dataset(trainset, args.num_train_shards, args.num_threads,
-                    'train', args.output_dir)
-    process_dataset(valset, args.num_val_shards, args.num_threads,
-                    'val', args.output_dir)
-    # Threaded method
+    dataset_dict = {'train': trainset, 'val': valset}
+
+    for phase in ['train', 'val']:
+
+        num_shards = args.num_train_shards if phase == 'train' \
+            else args.num_val_shards
+
+        # Split to shards.
+        dataset_shards = split_dataset(dataset_dict[phase], num_shards)
+
+        # Corresponding filenames of each shard.
+        filenames = get_filenames(args.output_dir, phase, num_shards)
+
+        # Writes TFRecords files.
+        process_dataset_v2(dataset_shards, filenames, args.num_cpu)
 
 
 if __name__ == "__main__":
