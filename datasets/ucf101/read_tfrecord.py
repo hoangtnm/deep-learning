194bf66e84debe1594316486642989a77e6fdab5
hoangtnm <minhhoangtrannhat@gmail.com>
2020-04-16 20:37:48 +0700

    Update UCF101 read_tfrecord

--- datasets/ucf101/read_tfrecord.py
+++ datasets/ucf101/read_tfrecord.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# from functools import partial
-
 import tensorflow as tf
 
 
@@ -10,47 +8,28 @@ def parse_fn(serialized_example):
 
     features = {
         'video': tf.io.FixedLenFeature([], tf.string),
-        'label': tf.io.FixedLenFeature([], tf.int64)
+        'label': tf.io.FixedLenFeature([], tf.int64),
+        'seq_len': tf.io.FixedLenFeature([], tf.int64),
+        'height': tf.io.FixedLenFeature([], tf.int64),
+        'width': tf.io.FixedLenFeature([], tf.int64),
+        'channels': tf.io.FixedLenFeature([], tf.int64)
     }
 
     # Parse the input tf.Example proto using the dictionary above.
     parsed = tf.io.parse_single_example(serialized_example, features)
-    return parsed
 
+    # Decodes and reshapes video
+    seq_len = tf.cast(parsed['seq_len'], tf.uint32)
+    height = tf.cast(parsed['height'], tf.uint32)
+    width = tf.cast(parsed['width'], tf.uint32)
+    channels = tf.cast(parsed['channels'], tf.uint32)
+    video = tf.io.decode_raw(parsed['video'], tf.uint8)
+    video = tf.reshape(video, shape=[seq_len, height, width, channels])
 
-# def extract_frames(example, frame_sampling: int):
-def extract_frames(example):
-    """Extract frames from video.
-
-    Args:
-        example: A dict of video and label.
-        frame_sampling: number of frames to sample.
-
-    Returns:
-        video, label: preprocessed batch.
-    """
-
-    video = tf.io.parse_tensor(example['video'], tf.uint8)
-    # num_frames = video.shape[0]
-    # frame_max = num_frames - frame_sampling
-    # frame_start = tf.random.uniform(
-    #     shape=[], maxval=frame_max, dtype=tf.int32
-    # )
-
-    # # Slices N frames starting from frame_start
-    # video = video[frame_start:frame_start+frame_sampling, :, :, :]
-    example['video'] = video
-    return example
-
-
-def normalize_img(example):
-    example['video'] = tf.cast(example['video'], tf.float32) / 255
-    example['label'] = tf.cast(example['label'], tf.float32)
-    return example
-
-
-def split_properties(example):
-    return example['video'], example['label']
+    # Normalizes video frames, label
+    video = tf.cast(video, tf.float32) / 255
+    label = tf.cast(parsed['label'], tf.float32)
+    return video, label
 
 
 def get_dataset(pattern, batch_size):
@@ -59,19 +38,10 @@ def get_dataset(pattern, batch_size):
         tf.data.TFRecordDataset,
         num_parallel_calls=tf.data.experimental.AUTOTUNE
     )
-    # dataset = dataset.shuffle(tf.data.experimental.AUTOTUNE)
+    # dataset = dataset.shuffle(buffer_size)
     # dataset = dataset.batch(batch_size)
     dataset = dataset.map(
         parse_fn, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.map(
-        extract_frames, num_parallel_calls=tf.data.experimental.AUTOTUNE
-    )
-    dataset = dataset.map(
-        normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE
-    )
-    dataset = dataset.map(
-        split_properties, num_parallel_calls=tf.data.experimental.AUTOTUNE
-    )
     dataset = dataset.batch(batch_size)
     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
     return dataset
