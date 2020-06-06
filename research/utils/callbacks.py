ec9705d457e4affd9a0e600d0e62fa1ec52bfa9f
hoangtnm <minhhoangtrannhat@gmail.com>
2020-06-06 20:20:37 +0700

    BREAKING CHANGE: rename get_default_callbacks to get_common_callbacks

--- research/utils/callbacks.py
+++ research/utils/callbacks.py
@@ -4,22 +4,26 @@ from tensorflow.keras.callbacks import (EarlyStopping, ModelCheckpoint,
                                         TensorBoard)
 
 
-def get_default_callbacks(log_dir, checkpoint_dir, **kwargs):
+def get_common_callbacks(log_dir, checkpoint_dir, monitor='val_accuracy',
+                         **kwargs):
     """Returns a list of common TensorFlow callbacks.
 
     Args:
         log_dir: Path to the directory
             where TensorBoard logs will be written.
         checkpoint_dir: Path to the directory
-            where model checkpoints will be written.
+            where checkpoints will be written.
+        monitor: Quantity to monitor.
+            Recommended values are `val_accuracy` or `val_loss`.
 
     Returns:
         A list of callbacks.
     """
     return [
+        TensorBoard(log_dir, **kwargs),
+        EarlyStopping(monitor=monitor, patience=3,
+                      restore_best_weights=True, **kwargs),
         ModelCheckpoint(os.path.join(checkpoint_dir, 'checkpoint'),
-                        monitor='val_loss', save_best_only=True,
+                        monitor=monitor, save_best_only=True,
                         save_weights_only=True, **kwargs),
-        EarlyStopping(monitor='val_accuracy', patience=3, **kwargs),
-        TensorBoard(log_dir, **kwargs)
     ]
