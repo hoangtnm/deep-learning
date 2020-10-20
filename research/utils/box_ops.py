20c30d6cee0bb77409d03cd373ceb8c434ce036c
hoangtnm <minhhoangtrannhat@gmail.com>
2020-10-20 10:08:34 +0700

    fix: box_ops docs

--- research/utils/box_ops.py
+++ research/utils/box_ops.py
@@ -4,7 +4,7 @@ from torch import Tensor
 
 
 def box_cxcywh_to_xyxy(x: Tensor) -> Tensor:
-    """Performs box-coordinates conversion, from COCO to Pascal VOC.
+    """Performs box-coordinates conversion.
 
     Args:
         x: Boxes are expected to be in (x, y, w, h) format.
@@ -23,13 +23,13 @@ def box_cxcywh_to_xyxy(x: Tensor) -> Tensor:
 
 
 def box_xyxy_to_cxcywh(x: Tensor) -> Tensor:
-    """Performs box-coordinates conversion, from Pascal VOC to COCO.
+    """Performs box-coordinates conversion, from Pascal VOC.
 
     Args:
         x: boxes are expected to be in (x1, y1, x2, y2) format.
 
     Returns:
-        Boxes in (x, y, w, h) format.
+        Boxes in (x_c, y_c, w, h) format.
 
     Shape:
         - input: (N, 4)
